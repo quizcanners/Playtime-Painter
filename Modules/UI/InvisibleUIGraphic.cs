6068e8e4381d4cbc03559f52f04a70b3981fbde8
quizcanners <quizcanners@gmail.com>
2021-01-07 22:44:27 +0200

    inspector for graphic

--- Modules/UI/InvisibleUIGraphic.cs
+++ Modules/UI/InvisibleUIGraphic.cs
@@ -1,12 +1,29 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using PlayerAndEditorGUI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace PlaytimePainter.UI
 {
 
-    public class InvisibleUIGraphic : Graphic
+    public class InvisibleUIGraphic : Graphic, IPEGI
     {
+        public bool Inspect()
+        {
+            var ico = raycastTarget;
+            if ("Raycast Target".toggleIcon(ref ico))
+                raycastTarget = ico;
+            return false;
+        }
+
         public override bool Raycast(Vector2 sp, Camera eventCamera) => true;
         protected override void OnPopulateMesh(VertexHelper vh) => vh.Clear();
     }
+    
+#if UNITY_EDITOR
+    [CustomEditor(typeof(InvisibleUIGraphic))]
+    public class InvisibleUIGraphicDrawer : PEGI_Inspector_Mono<InvisibleUIGraphic> { }
+#endif
 }
